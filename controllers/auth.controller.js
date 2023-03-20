5fd57ae68f385f9e539862883ae045de956b9429
Ella Byun <yantle@naver.com>
2023-03-20 22:23:56 +0900

    auth routes and auth controller setup

---
+++ controllers/auth.controller.js
@@ -0,0 +1,11 @@
+function getSignup(req, res){
+    res.render('signup');
+} 
+function getLogin(req, res){
+    res.render('login');
+} 
+
+module.exports = {
+ getSignup: getSignup,
+ getLogin: getLogin
+}
