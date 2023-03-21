784dfc35918a073e627afe8540e3a43b3b7b8139
Ella Byun <yantle@naver.com>
2023-03-21 22:39:15 +0900

    added user input validation feature

---
+++ util/validation.js
@@ -0,0 +1,34 @@
+const { post } = require("../routes/auth.routes")
+
+function isEmpty(value){
+    return (
+        !value ||
+        value.trim() === ''
+    )
+}
+
+function userCredentialAreValid(email, password) {
+    return email && 
+    email.includes('@') && 
+    password &&
+    password.trim().length >= 6 
+}
+
+function userDetailsAreValid(name, street, postal, city){
+    return (
+        userCredentialAreValid(email, password)&&
+        !isEmpty(name) &&
+        !isEmpty(street) &&
+        !isEmpty(postal) &&
+        !isEmpty(city)
+    )
+} 
+
+function emailIsConfirmed(email, confirmEmail){
+    return email === confirmEmail
+
+}
+module.exports = {
+    userDetailsAreValid: userDetailsAreValid,
+    emailIsConfirmed: emailIsConfirmed
+};
