8755d5e3788acbce6d1a0f2583399e590d443648
Ella Byun <yantle@naver.com>
2023-03-21 14:21:12 +0900

    added csrf token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,15 +1,29 @@
+const User = require('../models/user.model');
+
 function getSignup(req, res){
     res.render('customer/auth/signup');
 } 
 
-function signp(req, res){
-    
+async function signup(req, res){
+    const user = new User(
+        req.body.email, 
+        req.body.password, 
+        req.body.fullname, 
+        req.body.street, 
+        req.body.postal, 
+        req.body.city
+    );
+
+    await user.signup();
+
+    res.redirect('/login');
 }
 function getLogin(req, res){
-    res.render('login');
+    res.render('customer/auth/login');
 } 
 
 module.exports = {
  getSignup: getSignup,
- getLogin: getLogin
+ getLogin: getLogin,
+ signup: signup
 }
