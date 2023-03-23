37b089ff35ff9a3ba8ca82a513809f5124615adb
Ella Byun <yantle@naver.com>
2023-03-23 23:21:21 +0900

    added product update feature

--- middleswares/image-upload.js
+++ middleswares/image-upload.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const uuid = require('uuid').v4;
 const upload = multer({
     storage: multer.diskStorage({
-        destination: 'product-data/image',
+        destination: 'product-data/images',
         filename: function(req, file, cb){
             cb(null, uuid()+'-'+file.originalname);
         }
